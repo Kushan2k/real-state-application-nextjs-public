30b23f14b953c6b3e1d83177f58d77d76f0d24df
kushan2k <kushangayantha001@gmail.com>
2023-01-23 15:20:12 +0530

    env added

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
       "bayut-production.s3.eu-central-1.amazonaws.com",
     ],
   },
+  env: {
+    API_KEY: "f2ae02a8demsh8ef21a88a7afc66p175993jsnfa0b0f775c9b",
+  },
 }
 
 module.exports = nextConfig
